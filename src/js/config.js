ebfe24b89a2ebcf4f47f2b4d2644fd5038256c3e
self esteem <selfesteem@gmail.com>
2024-02-28 22:47:27 +0900

    final ...22

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,6 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = 'c6a71d97-f8c6-48cd-bc3d-6bdcc665f0c3';
+export const KEY2 = 'e2296584-4811-4b0c-9cde-33a8c4cc81ee';
 export const MODAL_CLOSE_SEC = 2.5;
